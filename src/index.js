6575d90f5a6167eb6da728cdbaba7115b0847ddd
Leonardo Casales <leonardo@smartnet.com.ar>
2021-11-28 20:41:02 -0300

    feat: ejs added

--- src/index.js
+++ src/index.js
@@ -2,30 +2,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.NODE_PORT || 3000;
-console.log('>>> port:');
-console.log(port);
 
+const port = process.env.NODE_PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public/views/home.html'));
-    });
+  res.render('index', { data: 'Hello World!' });
+});
 
-    const reloadSomeConfiguration = () => {
-        console.log('>>> reloadSomeConfiguration');
-    };
+const reloadSomeConfiguration = () => {
+  console.log('>>> reloadSomeConfiguration');
+};
 
-    process.once("SIGHUP", function () {
-        reloadSomeConfiguration();
-      })
+process.once("SIGHUP", function () {
+  reloadSomeConfiguration();
+})
 process.once('SIGUSR2', function () {
   // gracefulShutdown(function () {
-      console.log("killing...")
-    process.kill(process.pid, 'SIGUSR2');
+  console.log("killing...")
+  process.kill(process.pid, 'SIGUSR2');
   // });
 });
 
 app.listen(port || 3000, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 });
