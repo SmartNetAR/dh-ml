6a55d6c51d6b04ea7a0f7dd37d96f3976c305099
Leonardo Casales <leonardo@smartnet.com.ar>
2021-12-07 00:39:30 -0300

    Port env

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const setRoutes = require('./routes');
 const app = express();
 
-const port = process.env.NODE_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
